cc21166a0b25d50790fa8ea843ec9162a0a0df3f
INTERN <INTERN@Julio-Jaras.local>
2019-10-13 22:30:29 -0500

    prueba

--- metodosProyecto/src/metodosproyecto/randomForm.java
+++ metodosProyecto/src/metodosproyecto/randomForm.java
@@ -35,7 +35,12 @@ public class randomForm extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Método de los centros cuadrados");
+        jButton1.setText("Método de los centro cuadrados");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Método congruencial");
 
@@ -86,6 +91,10 @@ public class randomForm extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
